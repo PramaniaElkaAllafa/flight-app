be809df6bc7d119238638d17850c9ae7247122a8
PramaniaElkaAllafa <sayogyo182@gmail.com>
2020-02-15 15:27:29 +0700

    Tambah Edit User

--- includes/users.php
+++ includes/users.php
@@ -2,19 +2,61 @@
 Flight::route( 'POST /users/add', function(){
 	$db = Flight::db();
 
-	// $data = array(
-	// 	'username' => 'somenone',
-	// 	'password' => md5('password')
-	// );
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-	// $id = $db->insert('users', $data);
-	// if ($id)
-	//     echo 'user was created. Id=' . $id;
-	// else
-	//     echo 'insert failed: ' . $db->getLastError();
+	$data = array(
+		'username' => $username,
+		'password' => md5($password)
+	 );
+
+	$id = $db->insert('users', $data);
+ 	if ($id)
+ 		Flight::redirect( 'users');
+ 	 else
+ 	     echo 'insert failed: ' . $db->getLastError();
+ });
+
+Flight::route( 'POST /users/edit', function($username){
+	
+	$db = Flight::db();
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+
+	$data = array(
+		'username' => $username,
+		'password' => md5($password)
+	);
+	$db->where('username', $username);
+	$id = $db->insert('users', $data);
+	if($id)
+		Flight::redirect( 'users');
+	else
+		echo 'insert failed: ' . $db->getLastError();
 });
 
-Flight::route( 'GET /users(/page/@page:[0-9]+)', function($page){
+flight::route('/users/delete/@username', function($username){
+	$db = Flight::db();
+
+	$db->where('username', $username);
+	if($db->delete('users')) Flight::redirect ('users');
+
+});
+
+Flight::route( 'POST/users/edit/@username', function($username){
+	Flight::view()->set('title', 'Edit User');
+	Flight::render('edit-user', array(
+		'username' => $username
+		));
+});
+Flight::route( '/users/edit', function()
+	Flight::view()->set('title', 'Add Users');
+	Flight::render( 'users-app');
+});
+
+Flight::route('GET /users(/page/@page:[0-9]+)', function($page){
 	Flight::view()->set('title', 'Users');
 
 	if ( empty($page) ){
